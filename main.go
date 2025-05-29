e4d3321a0628d90803b85e1610b21161ec5ad6a9
Hamasah Fatiy Dakhilullah <hamasfa@gmail.com>
2025-05-29 15:49:16 +0700

    feat: configure CORS settings for improved security and access control

--- main.go
+++ main.go
@@ -88,7 +88,12 @@ func main() {
 	//setup fiber
 	app := fiber.New(configuration.NewFiberConfiguration())
 	app.Use(recover.New())
-	app.Use(cors.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:3000",
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowCredentials: true,
+	}))
 
 	// cronjob
 	cronJob := cron.New()
