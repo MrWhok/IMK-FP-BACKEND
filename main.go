b5569e8436e55669a70f1de70de3e8629b759f9f
Hamasah Fatiy Dakhilullah <hamasfa@gmail.com>
2025-06-25 10:26:13 +0700

    fix: update CORS configuration to change allowed origins

--- main.go
+++ main.go
@@ -94,7 +94,7 @@ func main() {
 	app := fiber.New(configuration.NewFiberConfiguration())
 	app.Use(recover.New())
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "https://hamasfa.fun, http://34.101.249.2:3000",
+		AllowOrigins:     "http://hamasfa.fun, http://api.hamasfa.fun",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
