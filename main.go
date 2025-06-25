960855897ebc855333ffc15a07876443b9e952a2
Hamasah Fatiy Dakhilullah <hamasfa@gmail.com>
2025-06-25 11:44:55 +0700

    fix: update CORS configuration to include localhost as an allowed origin

--- main.go
+++ main.go
@@ -94,7 +94,7 @@ func main() {
 	app := fiber.New(configuration.NewFiberConfiguration())
 	app.Use(recover.New())
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://34.101.249.2:3000, https://hoppscotch.io",
+		AllowOrigins:     "http://34.101.249.2:3000, https://hoppscotch.i, http://localhost:3000",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
